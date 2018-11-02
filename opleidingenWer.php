16549331b3e49c9f0ee52caa95ea4d6c756916df
StudentNiels <43601448+StudentNiels@users.noreply.github.com>
2018-11-02 14:54:06 +0100

    CSS  voor "opleidingen"
    experimenteren op pagina's
    (voorbeelden: https://www.han.nl/opleidingen/bachelor/werktuigbouwkunde/vt/)
    (https://www.nhlstenden.com/hbo-opleidingen/werktuigbouwkunde-bachelor-duaal)

--- opleidingenWer.php
+++ opleidingenWer.php
@@ -13,8 +13,11 @@
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
+    <!-- Global styles for this template -->
     <link href="css/global.css" rel="stylesheet">
+
+    <!-- Global styles for this template -->
+    <link href="css/opleidingenWer.css" rel="stylesheet">
   </head>
 
   <body>
@@ -27,16 +30,11 @@
         <div class="container">
             <div class="content">
                 <div class="row">
-                    <div class="col-sm-12 col-md-5">
+                    <div class="col-sm-12 col-md-12">
                         <div class="content-title">
                             <p><h1>Lorem ipsum</h1></p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
-                    </div>
-                    <div class="col-sm-12 col-md-7 mt-auto mb-auto">
-                        <div class="content-img">
-                            <img src="images/school.jpg" class="img-fluid">
-                        </div>
+                        <p id="introAlinea">Worden jouw werkzaamheden uitgebreid? Zoek je meer verdieping en wil je de ideale combinatie tussen werk, opleiding en privé? Kies dan voor de opleiding Werktuigbouwkunde duaal, een opleiding waarin werkend leren centraal staat.</p>
                     </div>
                 </div>
                 <div class="row">
@@ -45,6 +43,14 @@
                             <p><h2>Lorem ipsum</h2></p>
                         </div>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
+                    </div>
+                    <div class="col-sm-12 col-md-7 mt-auto mb-auto">
+                        <div class="content-img">
+                            <img src="images/school.jpg" class="img-fluid">
+                              <p>Lorem ipsum.</p>
+                            <img src="images/school.jpg" class="img-fluid">
+                              <p>Lorem ipsum.</p>
+                        </div>
                     </div>
                     <div class="col-sm-12 col-md-7">
                         <div class="content-subtitle">
