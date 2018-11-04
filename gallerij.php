91dcf39db3fc0f462d2f7a0dd04b8caa171e79be
LAPTOP-1IGV9KTA\Jarno <jarnohilverts@gmail.com>
2018-11-04 12:36:09 +0100

    gallerij werkend gemaakt + style gallerij + 2 folders voor de images van de gallerij

--- gallerij.php
+++ gallerij.php
@@ -1,4 +1,75 @@
 <!doctype html>
+<?php
+	
+	function mtimeuploaded($a, $b) 
+	{
+		$mtime_a = filemtime($a);
+		$mtime_b = filemtime($b);
+
+		if($mtime_a == $mtime_b)
+		{
+			return 0;
+		}
+		elseif($mtime_a < $mtime_b)
+		{
+			return -1;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	
+	$filestored = "images/stored/";
+	$images = glob($filestored."*.{jpg,gif,png}",GLOB_BRACE);
+	usort($images, "mtimeuploaded");
+	$images= array_reverse($images);
+	
+	$filestored 		= getcwd();
+	$uploadDirectory 	= "/images/uploaded/";
+	$errors 			= []; 
+	$fileExtensions 	= ['jpeg','jpg','png'];
+	
+	if (isset($_POST['submit']) && strlen($_FILES['myfile']['name']))
+	{		
+			$fileName 		= $_FILES['myfile']['name'];
+			$fileSize	 	= $_FILES['myfile']['size'];
+			$fileTmpName  	= $_FILES['myfile']['tmp_name'];
+			$fileType 		= $_FILES['myfile']['type'];
+			$fileExtension 	= strtolower(explode('.',$fileName)[1]);
+			$uploadPath 	= $filestored . $uploadDirectory . basename($fileName); 
+			
+			if (! in_array($fileExtension,$fileExtensions)) 
+			{
+				$errors[] = "Het bestandsformaat is niet toegestaan. Upload graag een bestand met het  bestandsformaat JPG, JPEG, png.";
+			}
+
+			if ($fileSize > 2000000) 
+			{
+				$errors[] = "De afbeelding is groter als 2 mb.";
+			}
+
+			if (empty($errors)) 
+			{
+				$didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+				if ($didUpload) 
+				{
+					echo "De afbeelding " . basename($fileName) . " is geupload.";
+				} 
+				else 
+				{
+					echo "Er is ergens een error opgetreden, probeer het opnieuw of neem contact op met de beheerder.";
+				}
+			} 
+			else 
+			{
+				foreach ($errors as $error) 
+				{
+					echo $error;
+				}
+			}
+		}
+	?>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -15,6 +86,7 @@
 
     <!-- Custom styles for this template -->
     <link href="css/global.css" rel="stylesheet">
+	<link href="css/gallerij.css" rel="stylesheet">
   </head>
 
   <body>
@@ -27,32 +99,25 @@
         <div class="container">
             <div class="content">
                 <div class="row">
-                    <div class="col-sm-12 col-md-5">
-                        <div class="content-title">
-                            <p><h1>Lorem ipsum</h1></p>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
-                    </div>
-                    <div class="col-sm-12 col-md-7 mt-auto mb-auto">
-                        <div class="content-img">
-                            <img src="images/school.jpg" class="img-fluid">
-                        </div>
-                    </div>
+					<?php
+						foreach ($images as $image) 
+						{
+							echo 	'<div class="col-6 col-sm-4 col-md-3 mt-2 mb-2">
+										<div class="content-img"> 
+											<div class="foto">
+												<img src="'.$image.'" class="img-fluid"/> 
+											</div>
+										</div> 
+									</div>';
+						}
+					?>
+					<form action="gallerij.php" method="post" enctype="multipart/form-data">
+				<p>Select image to upload:
+				<input type="file" name="myfile" id="fileToUpload">
+				<input type="submit" value="Upload Image" name="submit"></p>
+				</form>
                 </div>
-                <div class="row">
-                    <div class="col-sm-12 col-md-5">
-                        <div class="content-subtitle">
-                            <p><h2>Lorem ipsum</h2></p>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
-                    </div>
-                    <div class="col-sm-12 col-md-7">
-                        <div class="content-subtitle">
-                            <p><h2>Lorem ipsum</h2></p>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
-                    </div>
-                </div>
+				
             </div>
         </div>
     </main><!-- /.container -->
