5ce541f5e25fbb62908b6f283749a75157cb64b4
LAPTOP-1IGV9KTA\Jarno <jarnohilverts@gmail.com>
2018-11-04 19:32:02 +0100

    Gallerij foto's toegevoed

--- gallerij.php
+++ gallerij.php
@@ -27,49 +27,39 @@
 	
 	$filestored 		= getcwd();
 	$uploadDirectory 	= "/images/uploaded/";
-	$errors 			= []; 
+	$errors	 			= ""; 
 	$fileExtensions 	= ['jpeg','jpg','png'];
+	$response			= "";
 	
 	if (isset($_POST['submit']) && strlen($_FILES['myfile']['name']))
 	{		
-			$fileName 		= $_FILES['myfile']['name'];
-			$fileSize	 	= $_FILES['myfile']['size'];
-			$fileTmpName  	= $_FILES['myfile']['tmp_name'];
-			$fileType 		= $_FILES['myfile']['type'];
-			$fileExtension 	= strtolower(explode('.',$fileName)[1]);
-			$uploadPath 	= $filestored . $uploadDirectory . basename($fileName); 
+		$fileName 		= $_FILES['myfile']['name'];
+		$fileSize	 	= $_FILES['myfile']['size'];
+		$fileTmpName  	= $_FILES['myfile']['tmp_name'];
+		$fileType 		= $_FILES['myfile']['type'];
+		$fileExtension 	= strtolower(explode('.',$fileName)[1]);
+		$uploadPath 	= $filestored . $uploadDirectory . basename($fileName); 
+		
+		if (!in_array($fileExtension,$fileExtensions)) 
+		{
+			$errors = "Het bestandsformaat is niet toegestaan. Upload graag een bestand met het  bestandsformaat JPG, JPEG, png.";
+		}
+		
+		if (empty($errors))
+		{
+			$didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+			if ($didUpload) 
+			{
+				$response= "De afbeelding " . basename($fileName) . " is geupload.";
+			}
 			
-			if (! in_array($fileExtension,$fileExtensions)) 
-			{
-				$errors[] = "Het bestandsformaat is niet toegestaan. Upload graag een bestand met het  bestandsformaat JPG, JPEG, png.";
-			}
-
-			if ($fileSize > 2000000) 
-			{
-				$errors[] = "De afbeelding is groter als 2 mb.";
-			}
-
-			if (empty($errors)) 
-			{
-				$didUpload = move_uploaded_file($fileTmpName, $uploadPath);
-				if ($didUpload) 
-				{
-					echo "De afbeelding " . basename($fileName) . " is geupload.";
-				} 
-				else 
-				{
-					echo "Er is ergens een error opgetreden, probeer het opnieuw of neem contact op met de beheerder.";
-				}
-			} 
 			else 
 			{
-				foreach ($errors as $error) 
-				{
-					echo $error;
-				}
+				$response= "Er is ergens een error opgetreden, probeer het opnieuw of neem contact op met de beheerder.";
 			}
-		}
-	?>
+		}		
+	}
+?>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -104,18 +94,22 @@
 						{
 							echo 	'<div class="col-6 col-sm-4 col-md-3 mt-2 mb-2">
 										<div class="content-img"> 
-											<div class="foto">
-												<img src="'.$image.'" class="img-fluid"/> 
-											</div>
+												<img src="'.$image.'" alt="gallerij" class="img-fluid"/> 
 										</div> 
 									</div>';
 						}
 					?>
+				</div>
+				<div class="row">
 					<form action="gallerij.php" method="post" enctype="multipart/form-data">
 					<p>Select image to upload:
 					<input type="file" name="myfile" id="fileToUpload">
 					<input type="submit" value="Upload Image" name="submit"></p>
 					</form>
+					<?php
+						echo $response;
+						echo $errors;
+					?>
                 </div>
             </div>
         </div>
