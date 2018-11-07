8363f901cff28ab0650c83f831953307594495f5
sjihdazi <sjihdazi@gmail.com>
2018-11-07 12:56:04 +0100

    bedankt pagina

---
+++ bedankt.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js"
+         integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
+        <title>Bedankt</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/global.css" rel="stylesheet">
+    </head>
+
+  <body>
+    <?php
+        include 'components/menu.html';
+        include 'components/header.html';
+    ?>
+    <div class="seperator col-12"></div>
+    <main role="main" class="container">
+        <div class="container">
+            <div class="content">
+                <div class="row">
+                    <div class="col-sm-12 col-md-12">
+                        <div class="content-title">
+                            <p><h1 id="pageTitle">Bedankt!</h1></p>
+                        </div>
+                        <p>Voor het invullen van het formulier.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main><!-- /.container -->
+    <?php include 'components/footer.html'; ?>
+  </body>
+</html>
