92849af1498e502b519c1023a7f7a3852e9d7986
HFSchoonewille <hfschoonewille@gmail.com>
2018-11-07 20:58:33 +0100

    HTML contact formulier indentation

--- contact.php
+++ contact.php
@@ -21,99 +21,78 @@
         include 'components/header.html';
     ?>
     <div class="seperator col-12"></div>
-    <main role="main" class="container">
-        <div class="container">
-            <div class="content">
-                <div class="row">
-                    <div class="col-xl-6 col-sm-6">
-                    	<h1>Neem contact op</h1>
-                    	<form id="subscription_form" class="form-horizontal" action="bedankt.php" method="post">
-			            <!-- Name input-->
-			            <div class="form-group">
-			              <label class="control-label" for="name">naam</label>
-			              <div class="col-sm-9">
-			                <input id="name" name="name" type="text" placeholder="je naam" class="form-control">
-			              </div>
-			            </div>
-			            <!-- Email input-->
-			            <div class="form-group">
-			              <label class="control-label" for="email">E-mail</label>
-			              <div class="col-sm-9">
-			                <input id="email" name="email" type="text" placeholder="je email" class="form-control">
-			              </div>
-			            </div>
-			            <!-- Message body -->
-			            <div class="form-group">
-			              <label class="control-label" for="message">Je bericht</label>
-			              <div class="col-sm-9">
-			                <textarea class="form-control" id="message" name="message" placeholder="laat hier je bericht achter..." rows="5"></textarea>
-			              </div>
-			            </div>
+      <div class="container">
+        <div class="content">
+          <div class="row">
+            <div class="col-xl-6 col-sm-6">
+              <h1>Neem contact op</h1>
+              <form id="subscription_form" class="form-horizontal" action="bedankt.php" method="post">
+		            <!-- Name input-->
+		            <div class="form-group">
+		              <label class="control-label" for="name">Naam</label>
+		              <input id="name" name="name" type="text" placeholder="Je naam" class="form-control">
+		            </div>
+		            <!-- Email input-->
+		            <div class="form-group">
+		              <label class="control-label" for="email">E-mail</label>
+		               <input id="email" name="email" type="email" placeholder="Je email" class="form-control">
+		            </div>
+                <!-- Message body -->
+                <div class="form-group">
+                  <label class="control-label" for="message">Bericht</label>
+                  <textarea class="form-control" id="message" name="message" rows="5"></textarea>
+                </div>
+		            <p><u>En/of ik meld me aan voor de volgende opleiding(en):</u></p>
+		            <p><i>Werktuigbouwkunde :</i></p>
+		            <div class="custom-control custom-checkbox">
+    						  <input name="course[]" type="checkbox" class="custom-control-input" id="off-shore" value="off_shore">
+    						  <label class="custom-control-label" for="off-shore">Off-shore</label>
+    						</div>
+    						<div class="custom-control custom-checkbox">
+    						  <input name="course[]" type="checkbox" class="custom-control-input" id="scheepsbouw" value="scheepsbouw">
+    						  <label class="custom-control-label" for="scheepsbouw">Scheepsbouw</label>
+    						</div>
+    						<div class="custom-control custom-checkbox">
+    						  <input name="course[]" type="checkbox" class="custom-control-input" id="machine bouw" value="machine_bouw">
+    						  <label class="custom-control-label" for="machine bouw">Machine bouw</label>
+    						</div>
+    						<div class="custom-control custom-checkbox">
+    						  <input name="course[]" type="checkbox" class="custom-control-input" id="staal en constructie" value="staal_en_constructie">
+    						  <label class="custom-control-label" for="staal en constructie">Staal en Constructie</label>
+    						</div>
+    						<p><i>Informatica :</i></p>
+    						<div class="custom-control custom-checkbox">
+    						  <input name="course[]" type="checkbox" class="custom-control-input" id="ICT-beheer" value="ict_beheer">
+    						  <label class="custom-control-label" for="ICT-beheer">ICT-beheer</label>
+    						</div>
+    						<div class="custom-control custom-checkbox">
+    						  <input name="course[]" type="checkbox" class="custom-control-input" id="Software engineering" value="software_engineer">
+    						  <label class="custom-control-label" for="Software engineering">Software engineering</label>
+    						</div>
+    						<div class="custom-control custom-checkbox">
+    						  <input name="course[]" type="checkbox" class="custom-control-input" id=" technische informatica" value="technische_informatica">
+    						  <label class="custom-control-label" for=" technische informatica"> Technische Informatica</label>
+    						</div>
+    						<div class="custom-control custom-checkbox">
+    						  <input name="course[]" type="checkbox" class="custom-control-input" id="front end development" value="front_end_development">
+    						  <label class="custom-control-label" for="front end development">Front end Development</label>
+    						</div>
+		            <!-- Form actions -->
+		            <div class="form-group">
+		               <input name="submit"  type="submit" class="btn btn-primary btn-lg" value="inschrijven"></input>
+		            </div>
+		          </form>
+            </div>
 
-			            <p>Ik meld me aan voor de volgende opleiding(en)</p>
-			            <p><i>Werktuigbouwkunde :</i></p>
-			            <div class="custom-control custom-checkbox">
-						  <input name="course[]" type="checkbox" class="custom-control-input" id="off-shore" value="off_shore">
-						  <label class="custom-control-label" for="off-shore">Off-shore</label>
-						</div>
-						<div class="custom-control custom-checkbox">
-						  <input name="course[]" type="checkbox" class="custom-control-input" id="scheepsbouw" value="scheepsbouw">
-						  <label class="custom-control-label" for="scheepsbouw">Scheepsbouw</label>
-						</div>
-						<div class="custom-control custom-checkbox">
-						  <input name="course[]" type="checkbox" class="custom-control-input" id="machine bouw" value="machine_bouw">
-						  <label class="custom-control-label" for="machine bouw">Machine bouw</label>
-						</div>
-						<div class="custom-control custom-checkbox">
-						  <input name="course[]" type="checkbox" class="custom-control-input" id="staal en constructie" value="staal_en_constructie">
-						  <label class="custom-control-label" for="staal en constructie">Staal en Constructie</label>
-						</div>
-						<p><i>Informatica :</i></p>
-						<div class="custom-control custom-checkbox">
-						  <input name="course[]" type="checkbox" class="custom-control-input" id="ICT-beheer" value="ict_beheer">
-						  <label class="custom-control-label" for="ICT-beheer">ICT-beheer</label>
-						</div>
-						<div class="custom-control custom-checkbox">
-						  <input name="course[]" type="checkbox" class="custom-control-input" id="Software engineering" value="software_engineer">
-						  <label class="custom-control-label" for="Software engineering">Software engineering</label>
-						</div>
-						<div class="custom-control custom-checkbox">
-						  <input name="course[]" type="checkbox" class="custom-control-input" id=" technische informatica" value="technische_informatica">
-						  <label class="custom-control-label" for=" technische informatica"> Technische Informatica</label>
-						</div>
-						<div class="custom-control custom-checkbox">
-						  <input name="course[]" type="checkbox" class="custom-control-input" id="front end development" value="front_end_development">
-						  <label class="custom-control-label" for="front end development">Front end Development</label>
-						</div>
-			            <!-- Form actions -->
-			            <div class="form-group">			              
-			               <input name="submit"  type="submit" class="btn btn-primary btn-lg" value="inschrijven"></input>			              
-			            </div>
-			          </form>
-
-                    </div>
-
-                    <div class="col-xl-6 col-sm-6">
-                    	 <div class="content-img">
-                            <img src="images/school.jpg" class="img-fluid">
-                        </div>
-                    </div>
-
-                    <div class="content-title">
-                    	<div class="container">
-							<div class="row">
-						      <div class="col-sm-5 col-sm-offset-3">
-						        <div class="well well-sm">
-						          
-						        </div>
-						      </div>
-							</div>
-						</div>
-                    </div>
+            <div class="col-xl-6 col-sm-6">
+            	 <div class="content-img">
+                    <img src="images/school.jpg" class="img-fluid">
                 </div>
             </div>
+          </div>
         </div>
-    </main><!-- /.container -->
+      </div>
+    </div>
     <?php include "components/footer.html" ?>
   </body>
 </html>
