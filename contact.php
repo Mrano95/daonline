6977d1225f2a735d727abb49022e53cd7eedfc9b
HFSchoonewille <hfschoonewille@gmail.com>
2018-11-07 21:24:32 +0100

    Checkboxes vervangen voor radios per opleiding. Dit leek mij logischer

--- contact.php
+++ contact.php
@@ -24,8 +24,8 @@
       <div class="container">
         <div class="content">
           <div class="row">
-            <div class="col-xl-6 col-sm-6">
-              <h1>Neem contact op</h1>
+            <div class="col-xl-7 col-sm-12">
+              <h1>Neem contact op, of meld je aan</h1>
               <form id="subscription_form" class="form-horizontal" action="bedankt.php" method="post">
 		            <!-- Name input-->
 		            <div class="form-group">
@@ -44,37 +44,41 @@
                 </div>
 		            <p><u>En/of ik meld me aan voor de volgende opleiding(en):</u></p>
 		            <p><i>Werktuigbouwkunde :</i></p>
-		            <div class="custom-control custom-checkbox">
-    						  <input name="course[]" type="checkbox" class="custom-control-input" id="off-shore" value="off_shore">
+                <!-- <div class="custom-control custom-radio">
+                  <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
+                  <label class="custom-control-label" for="customRadio1">Toggle this custom radio</label>
+                </div> -->
+		            <div class="custom-control custom-radio">
+    						  <input name="courseWBK" type="radio" class="custom-control-input" id="off-shore" value="off_shore">
     						  <label class="custom-control-label" for="off-shore">Off-shore</label>
     						</div>
-    						<div class="custom-control custom-checkbox">
-    						  <input name="course[]" type="checkbox" class="custom-control-input" id="scheepsbouw" value="scheepsbouw">
+    						<div class="custom-control custom-radio">
+    						  <input name="courseWBK" type="radio" class="custom-control-input" id="scheepsbouw" value="scheepsbouw">
     						  <label class="custom-control-label" for="scheepsbouw">Scheepsbouw</label>
     						</div>
-    						<div class="custom-control custom-checkbox">
-    						  <input name="course[]" type="checkbox" class="custom-control-input" id="machine bouw" value="machine_bouw">
+    						<div class="custom-control custom-radio">
+    						  <input name="courseWBK" type="radio" class="custom-control-input" id="machine bouw" value="machine_bouw">
     						  <label class="custom-control-label" for="machine bouw">Machine bouw</label>
     						</div>
-    						<div class="custom-control custom-checkbox">
-    						  <input name="course[]" type="checkbox" class="custom-control-input" id="staal en constructie" value="staal_en_constructie">
+    						<div class="custom-control custom-radio">
+    						  <input name="courseWBK" type="radio" class="custom-control-input" id="staal en constructie" value="staal_en_constructie">
     						  <label class="custom-control-label" for="staal en constructie">Staal en Constructie</label>
     						</div>
     						<p><i>Informatica :</i></p>
-    						<div class="custom-control custom-checkbox">
-    						  <input name="course[]" type="checkbox" class="custom-control-input" id="ICT-beheer" value="ict_beheer">
+    						<div class="custom-control custom-radio">
+    						  <input name="courseICT" type="radio" class="custom-control-input" id="ICT-beheer" value="ict_beheer">
     						  <label class="custom-control-label" for="ICT-beheer">ICT-beheer</label>
     						</div>
-    						<div class="custom-control custom-checkbox">
-    						  <input name="course[]" type="checkbox" class="custom-control-input" id="Software engineering" value="software_engineer">
+    						<div class="custom-control custom-radio">
+    						  <input name="courseICT" type="radio" class="custom-control-input" id="Software engineering" value="software_engineer">
     						  <label class="custom-control-label" for="Software engineering">Software engineering</label>
     						</div>
-    						<div class="custom-control custom-checkbox">
-    						  <input name="course[]" type="checkbox" class="custom-control-input" id=" technische informatica" value="technische_informatica">
+    						<div class="custom-control custom-radio">
+    						  <input name="courseICT" type="radio" class="custom-control-input" id=" technische informatica" value="technische_informatica">
     						  <label class="custom-control-label" for=" technische informatica"> Technische Informatica</label>
     						</div>
-    						<div class="custom-control custom-checkbox">
-    						  <input name="course[]" type="checkbox" class="custom-control-input" id="front end development" value="front_end_development">
+    						<div class="custom-control custom-radio">
+    						  <input name="courseICT" type="radio" class="custom-control-input" id="front end development" value="front_end_development">
     						  <label class="custom-control-label" for="front end development">Front end Development</label>
     						</div>
 		            <!-- Form actions -->
@@ -84,7 +88,7 @@
 		          </form>
             </div>
 
-            <div class="col-xl-6 col-sm-6">
+            <div class="col-xl-5 col-sm-12">
             	 <div class="content-img">
                     <img src="images/school.jpg" class="img-fluid">
                 </div>
