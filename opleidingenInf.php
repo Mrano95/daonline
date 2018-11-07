3006200b58d9db1627f8b3eab188f2943ab1ce8c
StudentNiels <43601448+StudentNiels@users.noreply.github.com>
2018-11-07 13:21:57 +0100

    studierichtingen uit menu, afb opleidingen gecentreerd

--- opleidingenInf.php
+++ opleidingenInf.php
@@ -41,7 +41,7 @@
                         </div>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
                     </div>
-                    <div class="col-sm-12 col-md-10 mt-auto mb-auto  mr-auto ml-auto">
+                    <div class="col-sm-12 col-md-8 mt-auto mb-auto  mr-auto ml-auto">
                         <div class="col-sm-12 col-md-12 mt-auto mb-auto">
                             <img src="images/ITbredefoto.jpg" class="img-fluid">
                             <p class="col-sm-12 col-md-12">Afb.Werken met servers</p>
