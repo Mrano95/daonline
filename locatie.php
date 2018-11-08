90b240e3562e12ae50323ed9e7f7b7fa2858eebc
StudentNiels <43601448+StudentNiels@users.noreply.github.com>
2018-11-08 19:00:00 +0100

    iframe groter gemaakt
    wat kleine script indentjes verbeterd

--- locatie.php
+++ locatie.php
@@ -16,8 +16,6 @@
     <!-- global styles for this template -->
     <link href="css/global.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="css/global.css" rel="stylesheet">
   </head>
 
   <body>
@@ -36,14 +34,12 @@
                         </div>
                         <p>Drentse Academie</p>
                         <p>Het contact adres voor de Drentse Academie is in Leeuwarden</p>
-
                         <p>Adres</p>
                         <p>NHL Stenden Hogeschool Leeuwarden</p>
                         <p>Rengerslaan 8-10</p>
                         <p>8917 DD Leeuwarden</p>
                         <p>studentinfo@nhlstenden.com</p>
                         <p>+31 (0)88 991 7000</p>
-
                         <p>Postadres</p>
                         <p>Postbus 1080</p>
                         <p>8900 CB Leeuwarden</p>
@@ -52,7 +48,6 @@
                             <div class="content-subtitle">
                                 <p><h2>Onze locatie</h2></p>
                             </div>
-                            <div class="content-img">
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2413.4468314686014!2d6.910182015785215!3d52.778250379863856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e61f3ec72443%3A0xbe9d297b3e4fbcc7!2sNHL+Stenden+Hogeschool+Emmen!5e0!3m2!1snl!2snl!4v1541615745555" allowfullscreen></iframe>
                             </div>
                         </div>
