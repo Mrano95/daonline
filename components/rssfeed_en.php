69e5e7db0dcda3d9db6b2a7c9d380d9b771d4d8d
HFSchoonewille <hfschoonewille@gmail.com>
2018-11-08 12:09:42 +0100

    Titel rss vertaald voor Engelse weergave

---
+++ components/rssfeed_en.php
@@ -0,0 +1,22 @@
+<?php
+$amountOfArticles = 12;
+$feed = file_get_contents('http://feeds.newscientist.com/today-on-ns');
+// Parse data as XML
+$parsedXML = new SimpleXmlElement($feed);
+?>
+<div class="latest-news">
+  <h2>Latest news</h2>
+  <div class='row'>
+    <?php //foreach($parsedXML->channel->item as $entry)
+    for($x = 1; $x <= $amountOfArticles; $x++) : ?>
+      <div class="col-sm-4">
+        <div class="rss-item">
+          <a target="_blank" href="<?php echo $parsedXML->channel->item[$x]->link; ?>">
+            <h3><?php echo $parsedXML->channel->item[$x]->title; ?></h3>
+            <p><?php echo $parsedXML->channel->item[$x]->description; ?></p>
+          </a>
+        </div>
+      </div>
+    <?php endfor; ?>
+  </div>
+</div>
--- index_en.php
+++ index_en.php
@@ -56,7 +56,7 @@
             </div>
             <div class="row">
               <div class="col-sm-12 col-md-12">
-                <?php include 'components/rssfeed.php'; ?>
+                <?php include 'components/rssfeed_en.php'; ?>
               </div>
             </div>
         </div>
