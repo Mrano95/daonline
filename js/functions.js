34b32b285e87fb848b326858c0ca8089cee08338
Falko <hfschoonewille@gmail.com>
2018-11-09 12:08:12 +0100

    JS netjes

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,5 @@
-function langFunc(lang)
-{
-    // var lang = document.getElementById("lang_sel").value;
-    console.log(lang);
-    window.location = lang;
-}
-
 function applyTitle() {
     var pageTitle = document.getElementsByTagName("title")[0].innerHTML;
     var headerTitle = document.getElementById('page_title');
-    console.log(headerTitle);
     headerTitle.innerHTML = pageTitle;
 }
